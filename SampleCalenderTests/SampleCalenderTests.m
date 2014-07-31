d15dcf0334c9e745979e0c9d6a6488c2261f9ba9
ei17ringo <ei17ringo@gmail.com>
2014-07-31 10:29:56 +0800

    Initial Commit

---
+++ SampleCalenderTests/SampleCalenderTests.m
@@ -0,0 +1,34 @@
+//
+//  SampleCalenderTests.m
+//  SampleCalenderTests
+//
+//  Created by Eriko Ichinohe on 2014/07/31.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SampleCalenderTests : XCTestCase
+
+@end
+
+@implementation SampleCalenderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
