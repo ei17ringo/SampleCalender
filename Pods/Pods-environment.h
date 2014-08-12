73e7802962272b0c59d10067ea855a1c359c36d8
ei17ringo <ei17ringo@gmail.com>
2014-08-12 21:55:12 +0800

    init3

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// RDVCalendarView
+#define COCOAPODS_POD_AVAILABLE_RDVCalendarView
+#define COCOAPODS_VERSION_MAJOR_RDVCalendarView 1
+#define COCOAPODS_VERSION_MINOR_RDVCalendarView 0
+#define COCOAPODS_VERSION_PATCH_RDVCalendarView 7
+
