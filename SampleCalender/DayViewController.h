9c25b80f2d0ad57ef47b9c013de798eb8652c767
ei17ringo <ei17ringo@gmail.com>
2014-08-12 21:54:09 +0800

    init

---
+++ SampleCalender/DayViewController.h
@@ -0,0 +1,13 @@
+//
+//  DayViewController.h
+//  SampleCalender
+//
+//  Created by Eriko Ichinohe on 2014/08/12.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DayViewController : UIViewController
+
+@end
--- SampleCalender/ViewController.h
+++ SampleCalender/ViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "RDVCalendarViewController.h"
+#import "DayViewController.h"
 
 @interface ViewController : UIViewController
 
